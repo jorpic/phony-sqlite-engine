4e7298dbf60a67f2d7ec31687513b6bbf9733511
Max Taldykin <jorpic@gmail.com>
2022-11-17 17:40:21 +0400

    select from table

--- src/Engine.hs
+++ src/Engine.hs
@@ -11,7 +11,7 @@ module Engine
     ) where
 
 import Control.Lens
-import Control.Monad (when)
+import Control.Monad (when, void)
 import Control.Monad.Trans.Class (lift)
 import Control.Monad.Trans.Except
 import Control.Monad.Trans.State.Strict
@@ -19,19 +19,24 @@ import Data.Map (Map)
 import Data.Map qualified as Map
 import Data.Maybe (fromMaybe)
 import Data.Vector (Vector, (!?))
+import Data.Vector qualified as V
 
 -- FIXME: use phantom types instead? Id a = Id Int
-newtype CursorId = CursorId Int deriving (Eq, Ord, Show)
-newtype TableId = TableId Int deriving (Eq, Ord, Show)
+type CursorId = Int
+type TableId = String
 
 type Addr = Int
 data Val = StrVal String | IntVal Integer | RealVal Double
   deriving (Eq, Show)
-type Row = [Maybe Val]
-newtype Db = Db (Map TableId [Row])
+type Row = Vector (Maybe Val)
+type Table = Vector Row
+
+newtype Db = Db
+  { _dbData :: Map TableId Table
+  }
   deriving Show
 
-data Cursor = TableCursor TableId [Row]
+data Cursor = Cursor TableId Int
   deriving (Eq, Show)
 
 type EngineErr = String
@@ -46,6 +51,7 @@ data EngineState = EngineState
   }
   deriving Show
 
+makeLenses ''Db
 makeLenses ''EngineState
 
 defaultEngineState :: EngineState
@@ -70,23 +76,78 @@ data Cmd
     -- ^ The registers P1 through P1+P2-1 contain a single row of results.
   | OpenRead { cursor :: CursorId, table :: TableId }
   | Rewind { cursor :: CursorId, goto :: Addr }
+    -- ^ Rewind cursor P1. Jump to P2 if the table is empty.
+  | Column { cursor :: CursorId, column :: Int, reg :: Int, def :: Maybe Val }
+    -- ^ Read P2-th column from cursor P1, store result in P3, use P4 as
+    -- default.
+  | Next { cursor :: CursorId, goto :: Addr } -- _uniquenessHint :: Bool }
+    -- Advance P1, jump to P2 if not empty, P3 is a hint
+
   deriving (Eq, Show)
 
 type Engine a = StateT EngineState (Except EngineErr) a
 
 evalCmd :: Cmd -> Engine ()
 evalCmd = \case
-  Init{..} -> when (goto > 0) $ jumpTo .= Just goto
-  Goto{..} -> jumpTo .= Just goto
+  Init{..} -> when (goto > 0) $ jumpTo ?= goto
+  Goto{..} -> jumpTo ?= goto
   Halt -> lift $ throwE "Halt!"
-  Real{..}    -> registers %= Map.insert reg (RealVal rVal)
-  Integer{..} -> registers %= Map.insert reg (IntVal iVal)
-  String8{..} -> registers %= Map.insert reg (StrVal sVal)
+  Real{..}    -> registers %= (at reg ?~ RealVal rVal)
+  Integer{..} -> registers %= (at reg ?~ IntVal iVal)
+  String8{..} -> registers %= (at reg ?~ StrVal sVal)
+
   ResultRow{..} -> do
     regs <- use registers
-    let row = map (`Map.lookup` regs) [reg .. reg+len-1]
+    let row = V.fromList [regs ^. at r | r <- [reg .. reg+len-1]]
     yield .= [row]
+
+  OpenRead{..} -> do
+    void $ throwIfNothing (use $ db . dbData . at table)
+      $ "unknown table" ++ show table
+    throwIfJust (use $ openCursors . at cursor)
+      $ "cursor " ++ show cursor ++ " is already used"
+    openCursors %= (at cursor ?~ Cursor table 0)
+
+  Rewind{..} -> do
+    Cursor table _ <- getCursor cursor
+    -- rewind cursor
+    openCursors %= (at cursor ?~ Cursor table 0)
+    -- check if table is empty
+    row <- rowAt cursor
+    when (row == Nothing) $ jumpTo ?= goto
+
+  Column{..} -> do
+    row <- throwIfNothing (rowAt cursor)
+      $ "cursor overflow"
+    let val = fromMaybe def $ row !? column
+    registers %= (at reg .~ val)
+
+  Next{..} -> do
+    Cursor table offset <- getCursor cursor
+    openCursors %= (at cursor ?~ Cursor table (offset+1))
+    row <- rowAt cursor
+    when (row /= Nothing) $ jumpTo ?= goto
+
   _ -> lift $ throwE "Not implemented yet"
+
+
+throwIfJust :: Engine (Maybe a) -> EngineErr -> Engine ()
+throwIfJust f err = f >>= maybe (pure ()) (\_ -> lift $ throwE err)
+
+throwIfNothing :: Engine (Maybe a) -> EngineErr -> Engine a
+throwIfNothing f err = f >>= maybe (lift $ throwE err) pure
+
+getCursor :: CursorId -> Engine Cursor
+getCursor c
+  = throwIfNothing (use $ openCursors . at c)
+    $ "invalid cursor id" ++ show c
+
+rowAt :: CursorId -> Engine (Maybe Row)
+rowAt c = do
+  Cursor table offset <- getCursor c
+  tableData <- throwIfNothing (use $ db . dbData . at table)
+    $ "impossible! cursor for nonexistent table"
+  pure $ tableData !? offset
 
 
 type Query = Vector Cmd
@@ -101,7 +162,7 @@ evalToYield query = loop
         Nothing -> lift $ throwE $ "Invalid location: " ++ show _ip
         Just Halt -> return []
         Just cmd -> do
-          evalCmd cmd
+          evalCmd cmd -- FIXME: catch and add context to error
           EngineState{_jumpTo, _yield} <- get
           -- update instruction pointer
           ip .= fromMaybe (_ip + 1) _jumpTo
@@ -109,7 +170,7 @@ evalToYield query = loop
           -- loop if no yield
           case _yield of
             [] -> loop
-            _ -> return _yield
+            _ -> yield .= [] >> return _yield
 
 runWithDb :: Db -> Engine a -> Either EngineErr a
 runWithDb db' f = runExcept $ evalStateT f $ defaultEngineState { _db = db' }
--- test/EngineSpec.hs
+++ test/EngineSpec.hs
@@ -7,15 +7,38 @@ import Data.Vector qualified as V
 import Engine (runQuery, Db(..), Val(..), Cmd(..))
 
 
+testDb :: Db
+testDb = Db $ Map.fromList
+  [("users", V.fromList $ map (V.fromList . map Just)
+    [ [IntVal 1, StrVal "john doe", IntVal 42]
+    , [IntVal 2, StrVal "jane doe", IntVal 37]
+    ])
+  ]
+
 spec :: SpecWith ()
 spec = describe "engine" $ do
-  let db = Db Map.empty
-  let evalsTo q r = runQuery db (V.fromList q) `shouldBe` r
+  let evalsTo q r
+        = runQuery testDb (V.fromList q) `shouldBe` Right (map V.fromList r)
   it "evals trivial query"
-    $ [Init 0, Halt] `evalsTo` Right []
+    $ [Init 0, Halt] `evalsTo` []
   it "evals select constants "
     $ [ Init 0
       , Real 1 3.7, Integer 2 8, String8 3 "hello"
       , ResultRow 1 3
       ,  Halt
-      ] `evalsTo` Right [map Just [RealVal 3.7, IntVal 8, StrVal "hello"]]
+      ] `evalsTo` [map Just [RealVal 3.7, IntVal 8, StrVal "hello"]]
+
+  it "can select from table"
+    $ [ Init {goto = 0}
+      , OpenRead {cursor = 0, table = "users"}
+      , Rewind {cursor = 0, goto = 7}
+      ,  Column {cursor = 0, column = 0, reg = 1, def = Nothing}
+      ,  Column {cursor = 0, column = 1, reg = 2, def = Nothing}
+      ,  ResultRow {reg = 1, len = 2}
+      , Next {cursor = 0, goto = 3}
+      , Halt
+      ]
+      `evalsTo` (map (map Just))
+        [ [IntVal 1, StrVal "john doe"]
+        , [IntVal 2, StrVal "jane doe"]
+        ]
